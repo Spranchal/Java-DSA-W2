d1cd8389007e4d9084d67c943998b0fbbba3c4bd
Pranchal-Singh <spranchal864@gmail.com>
2025-12-23 21:12:23 +0530

    DAY5: Rest api

---
+++ journalApp/src/main/java/com/edigest/journalApp/Entity/JournalEntry.java
@@ -0,0 +1,18 @@
+package com.edigest.journalApp.Entity;
+
+public class JournalEntry {
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private String title;
+
+    private String content;
+}
