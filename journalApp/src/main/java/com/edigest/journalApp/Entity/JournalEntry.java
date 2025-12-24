31a10428dce42086d25a692ed9d859b1244b5780
Pranchal-Singh <spranchal864@gmail.com>
2025-12-24 19:27:57 +0530

    REST API SOME MORE

--- journalApp/src/main/java/com/edigest/journalApp/Entity/JournalEntry.java
+++ journalApp/src/main/java/com/edigest/journalApp/Entity/JournalEntry.java
@@ -14,5 +14,22 @@ public class JournalEntry {
 
     private String title;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
--- journalApp/src/main/java/com/edigest/journalApp/JournalApplication.java
+++ journalApp/src/main/java/com/edigest/journalApp/JournalApplication.java
@@ -9,5 +9,5 @@ public class JournalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JournalApplication.class, args);
 	}
-
+ 
 }
